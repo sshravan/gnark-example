28f01e6e1a7f7006fa87ba591cef1f4b9f424fe9
Shravan <sshravan@cs.umd.edu>
2022-06-29 12:27:49 -0400

    Removed duplicate code

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ import (
 	"github.com/sshravan/gnark-example/circuit/mimc"
 )
 
-func cubic_main() {
-	fmt.Println("Checks x**3 + x + 5 = y, where x is private and y is public")
+func driver(circuit, assignment frontend.Circuit) {
 
 	// Compiles the circuit into a R1CS
-	var circuit cubic.Circuit
-	ccs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &circuit)
+	ccs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, circuit)
 	assertNoError(err)
 
 	// Groth16: Setup
@@ -25,39 +23,7 @@ func cubic_main() {
 	assertNoError(err)
 
 	// Witness definition
-	assignment := cubic.Circuit{X: 3, Y: 35}
-	witness, err := frontend.NewWitness(&assignment, ecc.BN254)
-	assertNoError(err)
-
-	// Groth16: Prove
-	proof, err := groth16.Prove(ccs, pk, witness)
-	assertNoError(err)
-
-	// Groth16: Verify
-	publicWitness, _ := witness.Public()
-
-	err = groth16.Verify(proof, vk, publicWitness)
-	assertNoError(err)
-	fmt.Println("Proof verified")
-}
-
-func mimc_main() {
-	fmt.Println("Checks mimc(private preImage) = public hash")
-
-	// Compiles the circuit into a R1CS
-	var circuit mimc.Circuit
-	ccs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &circuit)
-	assertNoError(err)
-
-	// Groth16: Setup
-	pk, vk, err := groth16.Setup(ccs)
-	assertNoError(err)
-
-	// Witness definition
-	assignment := mimc.Circuit{
-		PreImage: "16130099170765464552823636852555369511329944820189892919423002775646948828469",
-		Hash:     "8674594860895598770446879254410848023850744751986836044725552747672873438975"}
-	witness, err := frontend.NewWitness(&assignment, ecc.BN254)
+	witness, err := frontend.NewWitness(assignment, ecc.BN254)
 	assertNoError(err)
 
 	// Groth16: Prove
@@ -73,8 +39,15 @@ func mimc_main() {
 }
 
 func main() {
-	mimc_main()
-	cubic_main()
+	var cublic_circuit cubic.Circuit
+	cubic_assignment := cubic.Circuit{X: 3, Y: 35}
+	driver(&cublic_circuit, &cubic_assignment)
+
+	var mimc_circuit mimc.Circuit
+	mimc_assignment := mimc.Circuit{
+		PreImage: "16130099170765464552823636852555369511329944820189892919423002775646948828469",
+		Hash:     "8674594860895598770446879254410848023850744751986836044725552747672873438975"}
+	driver(&mimc_circuit, &mimc_assignment)
 }
 
 func assertNoError(err error) {
