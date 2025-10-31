46cfae7d4493a6a16bdd83c6c876663deeb97af5
Shravan Srinivasan <ShravanSrinivasan1@gmail.com>
2025-10-31 13:58:03 -0400

    Updated tests

--- circuit/cubic/cubic_test.go
+++ circuit/cubic/cubic_test.go
@@ -3,24 +3,22 @@ package cubic
 import (
 	"testing"
 
-	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark/backend"
-	"github.com/consensys/gnark/examples/cubic"
 	"github.com/consensys/gnark/test"
 )
 
 func TestCubicEquation(t *testing.T) {
 	assert := test.NewAssert(t)
 
-	var cubicCircuit cubic.Circuit
+	var cubicCircuit Circuit
 
 	assert.ProverFailed(&cubicCircuit, &Circuit{
 		X: 42,
 		Y: 42,
-	}, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
+	})
 
 	assert.ProverSucceeded(&cubicCircuit, &Circuit{
 		X: 3,
 		Y: 35,
-	}, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
+	})
+
 }
