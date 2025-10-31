eb43db0f6af33d48b3072f4abec3005977173ea6
Shravan Srinivasan <ShravanSrinivasan1@gmail.com>
2025-10-31 14:21:32 -0400

    updated main with plonk

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/backend/plonk"
+	cs "github.com/consensys/gnark/constraint/bn254"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/test/unsafekzg"
 	"github.com/sshravan/gnark-example/circuit/cubic"
 	"github.com/sshravan/gnark-example/circuit/mimc"
 )
 
-func driver(circuit, assignment frontend.Circuit) bool {
+func driverGroth16(circuit, assignment frontend.Circuit) bool {
 
 	// Compiles the circuit into a R1CS
 	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, circuit)
@@ -35,20 +39,54 @@ func driver(circuit, assignment frontend.Circuit) bool {
 
 	err = groth16.Verify(proof, vk, publicWitness)
 	assertNoError(err)
-	fmt.Println("Proof verified")
+	fmt.Println("Groth16 proof verified")
+	return true
+}
+
+func driverPlonk(circuit, assignment frontend.Circuit) bool {
+	// Compiles the circuit into an SCS (for Plonk)
+	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, circuit)
+	assertNoError(err)
+
+	// Plonk: SRS and Setup (use same SRS for proving and verifying in this example)
+	scs := ccs.(*cs.SparseR1CS)
+	srs, srsLagrange, err := unsafekzg.NewSRS(scs)
+	assertNoError(err)
+
+	pk, vk, err := plonk.Setup(ccs, srs, srsLagrange)
+	assertNoError(err)
+
+	// Witness definition
+	witness, err := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
+	assertNoError(err)
+
+	// Plonk: Prove
+	proof, err := plonk.Prove(ccs, pk, witness)
+	assertNoError(err)
+
+	// Plonk: Verify
+	publicWitness, _ := witness.Public()
+
+	err = plonk.Verify(proof, vk, publicWitness)
+	assertNoError(err)
+	fmt.Println("Plonk proof verified")
 	return true
 }
 
 func main() {
 	var cubic_circuit cubic.Circuit
 	cubic_assignment := cubic.Circuit{X: 3, Y: 35}
-	driver(&cubic_circuit, &cubic_assignment)
+
+	driverGroth16(&cubic_circuit, &cubic_assignment)
+	driverPlonk(&cubic_circuit, &cubic_assignment)
 
 	var mimc_circuit mimc.Circuit
 	mimc_assignment := mimc.Circuit{
 		PreImage: "16130099170765464552823636852555369511329944820189892919423002775646948828469",
 		Hash:     "12886436712380113721405259596386800092738845035233065858332878701083870690753"}
-	driver(&mimc_circuit, &mimc_assignment)
+
+	driverGroth16(&mimc_circuit, &mimc_assignment)
+	driverPlonk(&mimc_circuit, &mimc_assignment)
 }
 
 func assertNoError(err error) {
